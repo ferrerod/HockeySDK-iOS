db7a40f0abaffd78862159a16a94f9791643e178
David Ferrero <ferrerod@users.noreply.github.com>
2025-04-01 11:38:58 -0600

    Link CrashReporter do not embed. Add Codesign for authenticity (#6)

--- makeXCFramework.sh
+++ makeXCFramework.sh
@@ -6,7 +6,7 @@ rm -rf archives/*
 rm -rf xcframeworks/*
 rmdir Vendor
 mkdir Vendor
-cp -a ../plCrashReporter/xcframeworks/CrashReporter.xcframework Vendor/
+ditto ../plCrashReporter/xcframeworks/CrashReporter.xcframework Vendor/CrashReporter.xcframework
 xcodebuild archive -project Support/HockeySDK.xcodeproj -scheme "HockeySDK Framework" -configuration "ReleaseCrashOnly" -destination "generic/platform=iOS" -archivePath "archives/HockeySDK-iOS"
 xcodebuild archive -project Support/HockeySDK.xcodeproj -scheme "HockeySDK Framework" -configuration "ReleaseCrashOnly" -destination "generic/platform=iOS Simulator" -archivePath "archives/HockeySDK-iOS_Simulator"
 # iOS only xcframework
@@ -16,7 +16,10 @@ xcodebuild archive -project Support/HockeySDK.xcodeproj -scheme "HockeySDK Frame
 #xcodebuild archive -project ../HockeySDK-Mac/Support/HockeySDK.xcodeproj -scheme "HockeySDK" -destination "generic/platform=macOS" -archivePath "archives/HockeySDK-macOS"
 #
 
-cp -a ../HockeySDK-Mac/archives/HockeySDK.xcarchive archives/HockeySDK-Mac.xcarchive
+ditto ../HockeySDK-Mac/archives/HockeySDK.xcarchive archives/HockeySDK-Mac.xcarchive
 
 # combine iOS, iOS Simulator and macOS archives into a single HockeySDK.xcframework
 xcodebuild -create-xcframework -archive archives/HockeySDK-iOS.xcarchive -framework HockeySDK.framework -archive archives/HockeySDK-iOS_Simulator.xcarchive -framework HockeySDK.framework -archive archives/HockeySDK-Mac.xcarchive -framework HockeySDK.framework -output xcframeworks/HockeySDK.xcframework
+
+# Codesign the xcframework for authenticity. It will get resigned by app developer.
+codesign -s "Apple Distribution: BugSplat, LLC (TN6R4Q475K)"  -f --timestamp xcframeworks/HockeySDK.xcframework
